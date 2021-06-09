96a9289f6ca90fb7b282c52f2cf85cd5a135d141
Krzysztof Kulig <kulig.krzysztof99@gmail.com>
2021-06-09 13:58:25 +0200

    Wgranie projektu na pierwszym etapie

---
+++ connectFourMain.py
@@ -0,0 +1,232 @@
+import sys
+import numpy as np
+import pygame
+pygame.init()
+
+# Definicja potrzebnych wymiarów
+ROWS = 6
+COLS = 7
+SQUARE_SIZE = 100
+width = COLS * SQUARE_SIZE
+height = (ROWS+2) * SQUARE_SIZE
+size = (width, height)
+RADIUS = int(SQUARE_SIZE/2 - 5)
+# Używane czcionki
+font = pygame.font.SysFont('Consolas',  40)
+font_2 = pygame.font.SysFont('Consolas',  60)
+activated = False
+pygame.display.set_caption("GRA CZTERY W RZEDZIE - Krzysztof Kulig")
+# Używane w grze kolory
+USED_COLORS = [
+    (255,255,255),
+    (255, 0, 0),
+    (255, 255, 0)
+]
+
+
+class Buttons():
+    
+    button_color = (158, 101, 168)
+    text_color = (255, 255, 255)
+    shadow_color = (165, 102, 176)
+    width = 50
+    height = 50
+
+    def __init__(self, x, y, text):
+        self.x = x
+        self.y = y
+        self.text = text
+
+    def set_buttons(self):
+        global activated
+        action = False
+        mouse_position = pygame.mouse.get_pos()
+        button = pygame.Rect(self.x, self.y, self.width, self.height)
+        # Warunek sprawdzający czy kursor jest nad przyciskiem
+        if button.collidepoint(mouse_position):
+            # Warunek sprawdzający czy przycisk został wciśnięty i puszczony
+            if pygame.mouse.get_pressed()[0] == 1:
+                activated = True
+                pygame.draw.rect(screen, self.button_color, button)
+            elif pygame.mouse.get_pressed()[0] == 0 and activated == True:
+                activated = False
+                action = True
+            else:
+                pygame.draw.rect(screen, self.button_color, button)
+        else:
+            pygame.draw.rect(screen, self.shadow_color, button)
+        # Przyciski - obramowanie
+        pygame.draw.line(screen, USED_COLORS[0], (self.x, self.y), (self.x+self.width, self.y), 2)
+        pygame.draw.line(screen, USED_COLORS[0], (self.x, self.y), (self.x, self.height+self.y), 2)
+        pygame.draw.line(screen, (120, 120, 120), (self.x, self.height+self.y), (self.x + self.width, self.height+self.y), 2)
+        pygame.draw.line(screen, (120, 120, 120), (self.x+self.width, self.y), (self.x+ self.width, self.height+self.y), 2)
+        # Przyciski - tekst
+        text_img = font.render(self.text, True, self.text_color)
+        text_len = text_img.get_width()
+        screen.blit(text_img, (self.x + int(self.width/2) - int(text_len/2), self.y+5))
+        return action
+
+# Przyciski nad kolumnami, wybór miejsca do wrzucenia monety
+button = []
+
+for i in range(0, COLS+1):
+    temp = str(i+1)
+    button.append(Buttons(25+i*100, 5, temp))
+
+
+def set_board(board):
+    for i in range(COLS+1):
+        for j in range(ROWS):
+            pygame.draw.rect(screen, (0, 0, 0), (i*SQUARE_SIZE, j*SQUARE_SIZE + SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+            pygame.draw.circle(screen, USED_COLORS[0], (int(i*SQUARE_SIZE+SQUARE_SIZE/2), int(j*SQUARE_SIZE + SQUARE_SIZE+SQUARE_SIZE/2)), RADIUS)
+
+# klasa z metodami logicznymi dotyczącymi gry
+class connect_four:
+    def __init__(self):
+        self.board = np.zeros((ROWS, COLS))
+
+    def put_coin(self,row_1, col_1, player):
+        self.board[row_1][col_1] = player
+
+    def full_col(self, col_1):
+        return self.board[ROWS - 1][col_1] == 0
+
+    def full_board(self):
+        counter = 0
+        for i in range(COLS):
+            if not self.board[ROWS - 1][i] == 0:
+                counter +=1
+        if counter == 7:
+            return True
+        return False
+
+    def free_row(self, col_1):
+        for i in range(ROWS):
+            if self.board[i][col_1] == 0:
+                return i
+
+    def show_board(self):
+        print(np.flip(self.board, 0))
+
+    def ktoWygral(self, player):
+        # Warunek sprawdzający czy są 4 monety w rzędzie poziomo
+        for column in range(COLS - 3):
+            for row in range(ROWS):
+                if self.board[row][column] == player and self.board[row][column + 1] == player and self.board[row][column + 2] == player and self.board[row][column + 3] == player:
+                    return True
+
+        # Warunek sprawdzający czy są 4 monety w rzędzie pionowo
+        for column in range(COLS):
+            for row in range(ROWS - 3):
+                if self.board[row][column] == player and self.board[row + 1][column] == player and self.board[row + 2][column] == player and self.board[row + 3][column] == player:
+                    return True
+
+        # Warunek sprawdzający czy są 4 monety w rzędzie na skos
+        for column in range(COLS - 3):
+            for row in range(ROWS - 3):
+                if self.board[row][column] == player and self.board[row + 1][column + 1] == player and self.board[row + 2][column + 2] == player and self.board[row + 3][column + 3] == player:
+                    return True
+        for column in range(COLS - 3):
+            for row in range(3, ROWS):
+                if self.board[row][column] == player and self.board[row - 1][column + 1] == player and self.board[row - 2][column + 2] == player and self.board[row - 3][column + 3] == player:
+                    return True
+
+app = connect_four()
+end = False
+turn = 0
+
+
+screen = pygame.display.set_mode(size)
+set_board(app.board)
+pygame.display.update()
+text = font.render("CHOOSE COLUMN", True, (255, 255, 255))
+screen.blit(text, (0, 70))
+m_1 = pygame.Rect(0, 700, 450, 120)
+info = pygame.Rect(100, 300, 500, 120)
+m_2 = pygame.Rect(350, 70, 400, 30)
+reset = Buttons(485, 720, "RESET")
+reset.width = 200
+reset.height = 50
+
+while not end:
+    pygame.draw.rect(screen, (0, 0, 0), m_1)
+    text_2 = font_2.render(f"PLAYER {turn + 1} TURN", True, USED_COLORS[turn +1])
+    screen.blit(text_2, (0, 720))
+    column = -1
+
+    for i in range(0, COLS):
+        if button[i].set_buttons():
+            column = i
+
+    if reset.set_buttons():
+        var = app.board
+        app = connect_four()
+        screen = pygame.display.set_mode(size)
+        set_board(app.board)
+        text = font.render("CHOOSE COLUMN", True, (255, 255, 255))
+        screen.blit(text, (0, 70))
+        pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    if app.full_board():
+        text = font.render("DRAW", True, USED_COLORS[0])
+        pygame.draw.rect(screen, (25, 190, 225), info)
+        pygame.draw.line(screen, USED_COLORS[0], (100, 300), (600, 300), 2)
+        pygame.draw.line(screen, USED_COLORS[0], (100, 300), (100, 420), 2)
+        pygame.draw.line(screen, (105, 105, 105), (100, 420), (600, 420), 2)
+        pygame.draw.line(screen, (105, 105, 105), (600, 300), (100 + 500, 420), 2)
+        screen.blit(text, (300, 340))
+        pygame.display.update()
+        pygame.time.wait(3000)
+        var = app.board
+        app = connect_four()
+        screen = pygame.display.set_mode(size)
+        set_board(app.board)
+        screen.blit(text, (0, 70))
+        pygame.display.update()
+
+    if column >= 0:
+        if app.full_col(column):
+            row = app.free_row(column)
+            app.put_coin(row, column, turn + 1)
+            pygame.draw.circle(screen, USED_COLORS[turn+1], (int(column * SQUARE_SIZE + SQUARE_SIZE / 2),int((5-row) * SQUARE_SIZE + SQUARE_SIZE + SQUARE_SIZE / 2)),RADIUS)
+
+            if app.ktoWygral(turn + 1):
+                pygame.draw.rect(screen, (0, 0, 0), m_1)
+                text_2 = font.render(f"PLAYER {turn + 1} WON!", True, (56, 54, 56))
+                text_3 = font.render(f"CONGRATULATIONS!", True, USED_COLORS[turn+1])
+                pygame.draw.rect(screen, (201, 182, 204), info)
+                pygame.draw.line(screen, USED_COLORS[0], (100, 300), (600, 300), 2)
+                pygame.draw.line(screen, USED_COLORS[0], (100, 300), (100, 420), 2)
+                pygame.draw.line(screen, (105, 105, 105), (100, 420),(600, 420), 2)
+                pygame.draw.line(screen, (105, 105, 105), (600, 300),(100 + 500, 420), 2)
+                screen.blit(text_2, (175, 320))
+                screen.blit(text_3, (225, 360))
+                pygame.display.update()
+                pygame.time.wait(3000)
+                var = app.board
+                app = connect_four()
+                screen = pygame.display.set_mode(size)
+                set_board(app.board)
+                screen.blit(text, (0, 70))
+                pygame.display.update()
+
+        else:
+            text_5 = font.render("COLUMN FULL", True, (201, 182, 204))
+            screen.blit(text_5, (400, 70))
+            pygame.display.update()
+            pygame.time.wait(1000)
+            pygame.draw.rect(screen, (0, 0, 0), m_2)
+            turn ^= 1
+
+
+
+        app.show_board()
+        turn ^= 1
+    pygame.display.update()
+
+
+
---
+++ opis_006.md
@@ -0,0 +1,31 @@
+Temat nr 6 - Cztery w rzędzie (https://en.wikipedia.org/wiki/Connect_Four)
+
+Opis zadania
+<ul>
+    <li> Okno wyświetlające siatkę 7 kolumn x 6 wierszy, przycisk nad każdą kolumną, informację "Tura gracza 1" lub "Tura gracza 2", przycisk do resetowania gry oraz rozwijalną listę wyboru reguł gry.
+    <li> Początkowo pola siatki są puste.
+    <li> Gracze na zmianę wrzucają monety do wybranych przez siebie kolumn.
+    <li> Pola w których jest moneta gracza 1 są czerwone, pola z monetami gracza 2 są żółte (tkinter, Canvas, http://stackoverflow.com/a/12254442).
+    <li> Gracze wybierają kolumnę klikając przycisk nad nią.
+    <li> Wygrywa gracz, który pierwszy ustawi cztery monety w linii (poziomo, pionowo lub po skosie).
+    <li> Gdy gra się kończy, wyświetlane jest okienko z napisem "Wygrał gracz 1" lub "Wygrał gracz 2", zależnie kto wygrał grę. Możliwe jest zresetowanie planszy bez zamykania głównego okna.
+    <li> Reprezentacja reguł gry ma być realizowana poprzez hierarchię klas. Klasa bazowa definiuje między innymi funkcję wirtualną ktoWygral() nadpisywaną w klasach pochodnych. Realizowane powinny być przynajmniej dwa zestawy reguł, jako dwie klasy pochodne.
+   </ul>
+Testy
+
+1. Wykonanie po dwa ruchy przez każdego z graczy - monety spadają na dół pola gry lub zatrzymują się na już wrzuconym żetonie.
+2. Ułożenie pionowej linii monet przez jednego gracza - oczekiwana informacja o jego wygranej.
+3. Ułożenie poziomej linii monet przez jednego gracza - oczekiwana informacja o jego wygranej.
+4. Ułożenie skośnej linii monet przez jednego gracza - oczekiwana informacja o jego wygranej.
+5. Zapełnienie pola gry tak, że żaden gracz nie ułożył linii - oczekiwana informacja o remisie.
+6. Ułożenie linii dłuższej niż 4 przez jednego z graczy - oczekiwana informacja o jego wygranej.
+[c][c][c][][c][c][c]<br>
+[ż][ż][ż][][ż][ż][ż] <- w następnym ruchu gracz żółty wrzuci monetę w środkową kolumnę
+7. Próba wrzucenia monety do zapełnionej kolumny - oczekiwana informacja o błędzie.
+
+https://github.com/kulig-krzysztof/JS_projekt
+
+
+```python
+
+```
